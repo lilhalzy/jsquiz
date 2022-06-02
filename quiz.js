6e00032f7dc8cfce4d10c9aae82b3c4ac6adf7c8
lilhalzy <lilhalzy11@gmail.com>
2022-06-02 20:44:30 +0800

    Added an array of questions

--- quiz.js
+++ quiz.js
@@ -4,3 +4,33 @@ const question_printed = document.getElementById("question");
 const answers = document.getElementById("answer_buttons");
 const next = document.getElementById("next");
 const body = document.getElementById("body");
+let shuffle, curr_num;
+
+const questions = [
+  {
+    question: 'What is this quizes for?',
+    answers: [
+      {text: 'JavaScript', correct: true},
+      {text: 'TypeScript', correct: false},
+      {text: 'Python', correct: false},
+    ]
+  },
+  {
+    question: 'How to become a great programmer?',
+    answers: [
+      {text: 'Become better at planting plants', correct: false},
+      {text: 'Always be on coding', correct: false},
+      {text: 'Always learn to code', correct: true},
+    ]
+  },
+  {
+    question: 'Do you love JavaScript?',
+    answers: [
+      {text: 'Forcing myself to love JavaScript', correct: true},
+      {text: 'JavaScript? What is that?', correct: false},
+      {text: 'I only know on how to center a div', correct: false},
+    ]
+  },
+]
+
+const array = ["2, 3", "3"]
