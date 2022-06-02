841e390a2734a023432a5af5c36141ae812c879c
lilhalzy <lilhalzy11@gmail.com>
2022-06-02 20:35:58 +0800

    Added constants to the quiz.js

---
+++ quiz.js
@@ -0,0 +1,6 @@
+const start = document.querySelector(".start_btn");
+const question_container = document.getElementById("question_container");
+const question_printed = document.getElementById("question");
+const answers = document.getElementById("answer_buttons");
+const next = document.getElementById("next");
+const body = document.getElementById("body");
